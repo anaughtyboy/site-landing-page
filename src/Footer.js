9c0f91e02c2386091e6e6ae464f375c7ca01a6a3
naughtyboy <94816625+anaughtyboy@users.noreply.github.com>
2022-07-27 15:25:04 +0800

    reset layout css

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,12 @@
 import './Footer.css'
 
-export default () => (
-  <footer className="footer">
-    <div className="container">
-      <span>All Rights Reserved ©2022&nbsp;&nbsp;陆地港二手车版权所有</span>
-      <a href="https://beian.miit.gov.cn/">皖ICP备2022006128号-1</a>
+const Footer = () => (
+  // <footer className="footer">
+    <div className="footer">
+      <span>All Rights Reserved ©2022&nbsp;&nbsp;版权所有</span>
       <span>安徽陆地港汽车销售有限公司</span>
+      <a href="https://beian.miit.gov.cn/">皖ICP备2022006128号-1</a>
+      {/* <span>安徽陆地港汽车销售有限公司</span> */}
       {/* <div style={{
         display: 'inline-flex',
         flexWrap: 'nowrap',
@@ -15,5 +16,6 @@ export default () => (
         <a href="">网警xxx</a>
       </div> */}
     </div>
-  </footer>
+  // </footer>
 )
+export default Footer;
